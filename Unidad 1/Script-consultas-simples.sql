5009a7e846840d6a8e66e9abc487c226846dc8f2
Efrain <efrain.areleon@gmailcom>
2024-01-24 16:58:33 -0600

    Commit con ejercicio Nortwiht terminado

---
+++ Unidad 1/Script-consultas-simples.sql
@@ -0,0 +1,9 @@
+-- Consultal simples con Select - SQL-LMD
+Use NORTHWND
+--Consulta numero 1 Seleccionar todos los clientes
+
+select * from Customers
+
+--Seleccionar el nombre del cliente, nombre del contacto y la ciudad
+
+select CompanyName, ContactName, City from Customers
